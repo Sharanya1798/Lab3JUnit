6367db676a9fd6e17b755d643abaf598efc19707
sharanya <Sharanya.Malyala@iiitb.org>
2021-03-11 22:13:39 +0530

    code added for squareroot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("What you want to test\nPress 1 for Email\nPress 2 for IP address\nPress 3 for Multiplication\n");
+        System.out.println("What you want to test\nPress 1 for Square Root\nPress 2 for IP address\nPress 3 for Multiplication\n");
         System.out.print("Enter your choice: ");
         Scanner scanner = new Scanner(System.in);
-        scanner.nextInt();
-        System.out.println("So do that, why bother this code ¯\\_(ツ)_/¯");
+        int choice = scanner.nextInt();
+        //System.out.println("So do that, why bother this code ¯\\_(ツ)_/¯");
+        if(choice == 1)
+        {
+            System.out.println("Enter any number");
+            double number = scanner.nextDouble();
+            System.out.println("Square root of given number is"+Math.sqrt(number));
+        }
 
     }
 }
