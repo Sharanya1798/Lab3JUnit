4f052f82a11ec4743375115f57499d1d6f1b2d9f
sharanya <Sharanya.Malyala@iiitb.org>
2021-03-12 11:01:18 +0530

    code added for Natural algoritm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("What you want to test\nPress 1 for Square Root\nPress 2 for Factorial function\nPress 3 for Multiplication\n");
+        System.out.println("What you want to test\nPress 1 for Square Root\nPress 2 for Factorial function\nPress 3 for Natural logarithm(ln(x))\n");
         System.out.print("Enter your choice: ");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
@@ -12,7 +12,7 @@ public class Main {
         {
             System.out.println("Enter any number");
             double number = scanner.nextDouble();
-            System.out.println("Square root of given number is"+Math.sqrt(number));
+            System.out.println("Square root of given number is "+Math.sqrt(number));
         } else if(choice == 2)
         {
             System.out.println("Enter a number for finding Factorial");
@@ -21,8 +21,12 @@ public class Main {
             for(i=1; i<=number; i++){
                 fact = fact*i;
             }
-            System.out.println("Factorial of given number is"+fact);
+            System.out.println("Factorial of given number is "+fact);
+        } else if(choice == 3)
+        {
+            System.out.println("Enter a Number");
+            double number = scanner.nextDouble();
+            System.out.println("Natural logorithm of the given number is "+Math.log(number));
         }
-
     }
 }
