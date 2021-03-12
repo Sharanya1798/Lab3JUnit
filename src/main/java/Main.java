a2b54cecc0531624fbcd7eb9d1802bf83ebb1e62
sharanya <Sharanya.Malyala@iiitb.org>
2021-03-12 11:09:25 +0530

    code added for Power function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("What you want to test\nPress 1 for Square Root\nPress 2 for Factorial function\nPress 3 for Natural logarithm(ln(x))\n");
+        System.out.println("What you want to test\nPress 1 for Square Root\nPress 2 for Factorial function\nPress 3 for Natural logarithm(ln(x))\nPress 4 for Power function\n");
         System.out.print("Enter your choice: ");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
@@ -26,7 +26,14 @@ public class Main {
         {
             System.out.println("Enter a Number");
             double number = scanner.nextDouble();
-            System.out.println("Natural logorithm of the given number is "+Math.log(number));
+            System.out.println("Natural logarithm of the given number is "+Math.log(number));
+        } else if(choice == 4)
+        {
+            System.out.println("Enter the base");
+            double base = scanner.nextDouble();
+            System.out.println("Enter the power");
+            double power = scanner.nextDouble();
+            System.out.println("The result obtained is "+Math.pow(base, power));
         }
     }
 }
